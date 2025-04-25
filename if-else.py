66be42a823ccc556bb084ca42fb0f6179fa2bcca
kauevasconcelos <kauefelipe1313@gmail.com>
2025-04-25 09:31:53 -0300

    Adição de mais um desafio no código

--- if-else.py
+++ if-else.py
@@ -6,3 +6,12 @@ if idade >= MAIOR_IDADE:
     print("Você pode tirar a sua CNH!")
 else:
     print("Você ainda não pode tirar sua primeira CNH!")
+
+# -----------------------------------------------
+
+ano = int(input("Digite o ano: "))
+
+if (ano % 4 == 0) and (ano % 100 != 0) or (ano % 400 == 0):
+    print(f'{ano} é um ano bissexto!')
+else:
+    print(f'{ano} não é um ano bissexto!')
