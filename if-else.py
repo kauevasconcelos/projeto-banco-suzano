249df40bcf10362752083bfe0a398ffa2ae02d22
kauevasconcelos <kauefelipe1313@gmail.com>
2025-04-25 09:19:02 -0300

    primeiro commit com código

---
+++ if-else.py
@@ -0,0 +1,8 @@
+MAIOR_IDADE = 18
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Você pode tirar a sua CNH!")
+else:
+    print("Você ainda não pode tirar sua primeira CNH!")
